f71f4e782925af0d0e56eb60e773e88ee844336f
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-12 14:51:37 +0300

    onClick method

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import React from 'react';
 import './assets/css/style.css';
 
 //fonksiyonel component
-function App({ title }) {
-  return (
-    <div>
-      <div className='bg-gray-600 text-white p-5 border'>{title}</div>
-    </div>
-  );
-}
+// function App({ title }) {
+//   return (
+//     <div>
+//       <div className='bg-gray-600 text-white p-5 border'>{title}</div>
+//     </div>
+//   );
+// }
 
 //sınıf component
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-//     //this App componentinden referans alıyor
-//     this.state = { title: 'Hello Constructor' };
-//   }
-//   render() {
-//     return <div>{this.state.title} </div>;
-//   }
-// }
+    //this App componentinden referans alıyor
+    this.state = { title: 'Hello Constructor', isShowing: false };
+  }
+  render() {
+    return (
+      <section className='flex justify-center'>
+        <div className='w-1/2'>
+          <div className='my-4'>{this.state.title}</div>
+          <div className='text-center'>
+            <button
+              className='p-1 bg-blue-700 text-white my-2'
+              onClick={() => {
+                this.setState({ isShowing: !this.state.isShowing });
+              }}
+            >
+              Toogle Image
+            </button>
+          </div>
+          {this.state.isShowing ? (
+            <img src='https://cdn.pixabay.com/photo/2020/06/07/17/19/child-5271290_960_720.jpg' />
+          ) : null}
+        </div>
+      </section>
+    );
+  }
+}
 export default App;
