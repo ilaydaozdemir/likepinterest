04d352f4aaae88afaf0f18e516154a5879a2ee32
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-12 20:24:35 +0300

    componentDidMount()

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 export default class Images extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { interval: null };
+  }
+
+  componentDidMount() {
+    console.log('Images Comp Mounted');
+    this.setState({
+      interval: setInterval(() => {
+        console.log('hello');
+      }, 1000),
+    });
+  }
+
   componentWillUnmount() {
     console.log('Images Comp Unmounted');
+    clearInterval(this.state.interval);
   }
   render() {
     return (
