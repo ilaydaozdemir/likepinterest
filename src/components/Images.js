576c797924be668f4c1812c070f7d8c6cea574df
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-14 20:56:51 +0300

    try clear code

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Image from './image';
 
 export default function Images() {
   const [images, setimages] = useState([
@@ -19,30 +20,11 @@ export default function Images() {
     ]);
   }
 
-  const [isHovering, setIsHovering] = useState(-1);
-
   //inner Component
   function ShowImage(params) {
-    return images.map((image, index) => {
-      return (
-        <div className='w-1/3 my-4 flex justify-center' key={index}>
-          <div
-            className='relative'
-            onMouseEnter={() => setIsHovering(index)}
-            onMouseLeave={() => setIsHovering(-1)}
-          >
-            {/* icon ekleme */}
-            <i
-              className={`fas fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100 ${
-                isHovering === index ? '' : 'hidden'
-              }`}
-              onClick={() => handleRemove(index)}
-            ></i>
-            <img src={image} width='150' />
-          </div>
-        </div>
-      );
-    });
+    return images.map((img, index) => (
+      <Image image={img} handleRemove={handleRemove} index={index} />
+    ));
   }
 
   // improve separate
