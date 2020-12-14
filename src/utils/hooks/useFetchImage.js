87af26e2ef64d71afca302f0aef60b4912c53a6a
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-15 01:06:49 +0300

    structure another page

--- src/utils/hooks/useFetchImage.js
+++ src/utils/hooks/useFetchImage.js
@@ -4,12 +4,12 @@ import Axios from 'axios';
 const url = process.env.REACT_APP_UNSPLASH_URL;
 const secret = process.env.REACT_APP_UNSPLASH_KEY;
 
-export default function useFetchImage() {
+export default function useFetchImage(page) {
   const [images, setImages] = useState([]);
   useEffect(() => {
-    Axios.get(`${url}?client_id=${secret}`).then(res => {
+    Axios.get(`${url}?client_id=${secret}&page=${page}`).then(res => {
       setImages(res.data);
     });
-  }, []);
+  }, [page]);
   return [images, setImages];
 }
