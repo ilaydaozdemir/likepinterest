867d8d716560f7c93376b190c9c56eb380427905
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-14 20:32:16 +0300

    onMouseEnter & onMouseLeave

--- src/components/Images.js
+++ src/components/Images.js
@@ -19,17 +19,33 @@ export default function Images() {
     ]);
   }
 
+  const [isHovering, setIsHovering] = useState(false);
+  function handleMouseEnter() {
+    setIsHovering(true);
+  }
+  function handleMouseLeave() {
+    setIsHovering(false);
+  }
+
   //inner Component
   function ShowImage(params) {
     return images.map((image, index) => {
       return (
         <div className='w-1/3 my-4 flex justify-center' key={index}>
           <div className='relative'>
+            {/* icon ekleme */}
             <i
-              className='fas fa-times absolute right-0 cursor-pointer'
+              className={`fas fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100 ${
+                isHovering ? '' : 'hidden'
+              }`}
               onClick={() => handleRemove(index)}
             ></i>
-            <img src={image} width='150' />
+            <img
+              src={image}
+              width='150'
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
           </div>
         </div>
       );
