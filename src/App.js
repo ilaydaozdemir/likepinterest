c93ebdb83e995e1ec67ee4c6623a7fd826db5874
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-18 23:51:38 +0300

    optimize router

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './assets/css/style.css';
-import Gallery from './page/Gallery';
-import Home from './page/Home';
-import Login from './page/Login';
+import routes from './utils/routes';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' exact={true}>
-          <Home />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/gallery'>
-          <Gallery />
-        </Route>
+        {routes.map(route => (
+          <Route
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+          />
+        ))}
       </Switch>
     </Router>
   );
