542000d8eba4d1e3fc25503805e068c6116ebb85
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-18 23:33:06 +0300

    optimize router

--- src/App.js
+++ src/App.js
@@ -2,35 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './assets/css/style.css';
-import Images from './components/Images';
-
-function Gallery() {
-  return (
-    <section className='flex justify-center'>
-      <div className='w-10/12'>
-        <div className='text-center'>
-          <Images />
-        </div>
-      </div>
-    </section>
-  );
-}
-
-function Home() {
-  return (
-    <div className='flex h-screen'>
-      <h1 className='m-auto text-3xl'>Welcome Home</h1>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div className='flex h-screen'>
-      <h1 className='m-auto text-3xl'>Login Page</h1>
-    </div>
-  );
-}
+import Gallery from './page/Gallery';
+import Home from './page/Home';
+import Login from './page/Login';
 
 function App() {
   return (
