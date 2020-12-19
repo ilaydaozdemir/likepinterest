b5314c0d6dccd5bde609f9040e0d3d780bf1d5a6
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-19 19:29:47 +0300

    adding firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBG93wm30eDtYXV2_QElK3EcNhXuT8thfA',
+  authDomain: 'in-depth-react-21a92.firebaseapp.com',
+  projectId: 'in-depth-react-21a92',
+  storageBucket: 'in-depth-react-21a92.appspot.com',
+  messagingSenderId: '831283409396',
+  appId: '1:831283409396:web:9defc11b1ed8ae0a6cc9a1',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
