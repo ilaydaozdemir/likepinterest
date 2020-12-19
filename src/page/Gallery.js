187a91fab0a80814aa903d085de212f354dcad04
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-19 03:59:11 +0300

    design

--- src/page/Gallery.js
+++ src/page/Gallery.js
@@ -3,7 +3,7 @@ import Images from '../components/Images';
 
 export default function Gallery() {
   return (
-    <section className='flex justify-center'>
+    <section className='flex justify-center '>
       <div className='w-10/12'>
         <div className='text-center'>
           <Images />
