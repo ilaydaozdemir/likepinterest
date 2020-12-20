ab9f8ce383c220c5aa0d4182d6f192d1390e1d2c
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-20 23:06:09 +0300

    clean code

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -1,64 +1,62 @@
-import { useFormik } from 'formik';
+import { ErrorMessage, Field, Form, Formik, useFormik } from 'formik';
 import React, { useState } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
 import firebase from '../config/firebase';
 import * as Yup from 'yup';
 
 export default function SignUp() {
-  const formik = useFormik({
-    initialValues: { email: '', password: '' },
-    onSubmit: value => {
-      console.log(value);
-    },
-
-    validationSchema: Yup.object({
-      email: Yup.string().required('Email is required').email(),
-      password: Yup.string().required('Password is required').min(6),
-    }),
-  });
   return (
-    <div className='flex h-screen lg bg-gradient-to-r from-red-100 via-red-200 to-red-100'>
-      <div className='m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-3xl bg-gradient-to-r from-red-300 via-red-400 to-red-300'>
-        <form className='m-5 w-10/12' onSubmit={formik.handleSubmit}>
-          <h1 className='w-full text-4xl tracking-widest text-center my-6 '>
-            Sign Up Here
-          </h1>
-          <div className='w-full my-6'>
-            <input
-              type='email'
-              className='p-2 rounded shadow w-full text-black'
-              placeholder='Email or Username'
-              {...formik.getFieldProps('email')}
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <p>{formik.errors.email}</p>
-            ) : null}
-          </div>
-          <div className='w-full my-6'>
-            <input
-              type='password'
-              className='p-2 rounded shadow w-full text-black'
-              placeholder='Password'
-              {...formik.getFieldProps('password')}
-            />
-            {formik.touched.password && formik.errors.password ? (
-              <p>{formik.errors.password}</p>
-            ) : null}
-          </div>
-          <div className='w-full my-10'>
-            <button
-              type='submit'
-              className='p-2 rounded shadow w-full bg-gradient-to-r from-gray-700 to-red-400 hover:from-pink-500 hover:to-yellow-500  text-white'
-            >
-              {/* {isLoading ? (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      onSubmit={value => {
+        console.log(value);
+      }}
+      validationSchema={Yup.object({
+        email: Yup.string().required('Email is required').email(),
+        password: Yup.string().required('Password is required').min(6),
+      })}
+    >
+      <div className='flex h-screen lg bg-gradient-to-r from-red-100 via-red-200 to-red-100'>
+        <div className='m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-3xl bg-gradient-to-r from-red-300 via-red-400 to-red-300'>
+          <Form className='m-5 w-10/12'>
+            <h1 className='w-full text-4xl tracking-widest text-center my-6 '>
+              Sign Up Here
+            </h1>
+            <div className='w-full my-6'>
+              <Field
+                name='email'
+                type='email'
+                className='p-2 rounded shadow w-full text-black'
+                placeholder='Email or Username'
+              />
+
+              <ErrorMessage name='email' />
+            </div>
+            <div className='w-full my-6'>
+              <Field
+                name='password'
+                type='password'
+                className='p-2 rounded shadow w-full text-black'
+                placeholder='Password'
+              />
+
+              <ErrorMessage name='password' />
+            </div>
+            <div className='w-full my-10'>
+              <button
+                type='submit'
+                className='p-2 rounded shadow w-full bg-gradient-to-r from-gray-700 to-red-400 hover:from-pink-500 hover:to-yellow-500  text-white'
+              >
+                {/* {isLoading ? (
                 <i className='fas fa-circle-notch fa-spin'></i>
               ) : ( */}
-              Sign Up
-              {/* )} */}
-            </button>
-          </div>
-        </form>
+                Sign Up
+                {/* )} */}
+              </button>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div>
+    </Formik>
   );
 }
