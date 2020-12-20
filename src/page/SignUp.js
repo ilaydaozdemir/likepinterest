ec54f9c62f43a55874b8f701b17ae3946d69016b
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-20 22:37:10 +0300

    formik yerine Yup kullanımı

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -2,6 +2,7 @@ import { useFormik } from 'formik';
 import React, { useState } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
 import firebase from '../config/firebase';
+import * as Yup from 'yup';
 
 export default function SignUp() {
   const formik = useFormik({
@@ -10,24 +11,10 @@ export default function SignUp() {
       console.log(value);
     },
 
-    //submit butınuna dahı basmamıza ızın vermez
-    validate: values => {
-      const errors = {};
-      if (!values.email) {
-        errors.email = 'Email Field is Required.';
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-      ) {
-        errors.email = 'Email is invalid.';
-      }
-
-      if (!values.password) {
-        errors.password = 'Password Field is Required.';
-      } else if (!values.password.length <= 6) {
-        errors.password = 'Password must be longer than 6.';
-      }
-      return errors;
-    },
+    validationSchema: Yup.object({
+      email: Yup.string().required('Email is required').email(),
+      password: Yup.string().required('Password is required').min(6),
+    }),
   });
   return (
     <div className='flex h-screen lg bg-gradient-to-r from-red-100 via-red-200 to-red-100'>
@@ -49,7 +36,6 @@ export default function SignUp() {
             {formik.touched.email && formik.errors.email ? (
               <p>{formik.errors.email}</p>
             ) : null}
-            <p> {formik.errors.email}</p>
           </div>
           <div className='w-full my-6'>
             <input
@@ -64,7 +50,6 @@ export default function SignUp() {
             {formik.touched.password && formik.errors.password ? (
               <p>{formik.errors.password}</p>
             ) : null}
-            <p> {formik.errors.password}</p>
           </div>
           <div className='w-full my-10'>
             <button
