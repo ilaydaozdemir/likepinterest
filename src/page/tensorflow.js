8a5fd9eaa46a7c542eb925c5e6685e3d747fc23a
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-25 14:54:25 +0300

    resmı okutabılıyorum

--- src/page/tensorflow.js
+++ src/page/tensorflow.js
@@ -5,25 +5,35 @@ import * as mobilenet from '@tensorflow-models/mobilenet';
 export default function Tensorflow() {
   const imageRef = useRef();
 
-  useEffect(() => {
+  function predict() {
     const img = imageRef.current;
 
-    // mobilenet.load().then(model => {
-    //   model.classify(img).then(predictions => {
-    //     console.log('Predictions:');
-    //     console.log(predictions);
-    //   });
-    // });
-  }, []);
+    mobilenet.load().then(model => {
+      model.classify(img).then(predictions => {
+        console.log('Predictions:');
+        console.log(predictions);
+      });
+    });
+  }
+
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center '>
       <div className='w-1/3'>
         <h1 className='text-center'>Tensorflow Example</h1>
         <img
           src='https://images.unsplash.com/photo-1608821328028-679ca8b76080?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxOTA3OTV8MHwxfGFsbHw0fHx8fHx8Mnw&ixlib=rb-1.2.1&q=80&w=1080'
           width='400'
+          crossOrigin='anonymous'
           ref={imageRef}
         />
+        <div className='text-center my-5'>
+          <button
+            className='p-2 rounded shadow  bg-gradient-to-r from-gray-700 via-gray-900 to-gray-700 text-white'
+            onClick={predict}
+          >
+            Predict Result
+          </button>
+        </div>
       </div>
     </div>
   );
